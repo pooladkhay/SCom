597cbd1b83f78ceaf39afe7317962d43bbb6915c
MJ Pooladkhay <mj@pky.me>
2024-07-07 22:26:35 +0100

    cleanup tests-driver.scm
    Signed-off-by: MJ Pooladkhay <mj@pky.me>

--- tests/tests-driver.scm
+++ tests/tests-driver.scm
@@ -1,4 +1,3 @@
-
 (define all-tests '())
 
 (define-syntax add-tests-with-string-output
@@ -9,28 +8,10 @@
            '(test-name [expr string  output-string] ...)
             all-tests))]))
 
-(define (run-compile expr)
-  (let ([p (open-output-file "stst.s" 'replace)])
-    (compile-program expr p)
-    (close-output-port p)))
-
-(define (build)
-  (unless (zero? (system "gcc -o stst startup.c stst.s"))
-    (error 'make "could not build target")))
-
-(define (execute)
-  (unless (zero? (system "./stst > stst.out"))
-    (error 'make "produced program exited abnormally")))
-
-
-(define (build-program expr)
-   (run-compile expr)
-   (build))
-
 (define (get-string)
   (with-output-to-string
     (lambda ()
-      (with-input-from-file "stst.out"
+      (with-input-from-file "program.out"
         (lambda ()
           (let f ()
             (let ([c (read-char)])
@@ -73,22 +54,6 @@
                  (test-one i (car tests))
                  (g (add1 i) (cdr tests))])))))))
 
-
-(define input-filter 
-  (make-parameter (lambda (x) x)
-    (lambda (x)
-      (unless (procedure? x)
-        (error 'input-filter "not a procedure ~s" x))
-      x)))
-
-(define runtime-file 
-  (make-parameter
-    "runtime.c"
-    (lambda (fname)
-      (unless (string? fname) (error 'runtime-file "not a string" fname))
-      fname)))
-
-
 (define compile-port
   (make-parameter
     (current-output-port)
@@ -97,39 +62,20 @@
          (error 'compile-port "not an output port ~s" p))
        p)))
 
-(define show-compiler-output (make-parameter #f))
-
 (define (run-compile expr)
-  (let ([p (open-output-file "stst.s" 'replace)])
+  (let ((p (open-output-file "code.s" 'replace)))
     (parameterize ([compile-port p])
-       (compile-program expr))
+       (emit-assembly expr)) ; implemented by compiler.scm
     (close-output-port p)))
 
+(define (build)
+  (unless (fxzero? (system "gcc -o program runtime.c code.s"))
+    (error 'make "could not build target")))
 
 (define (execute)
-  (unless (fxzero? (system "./stst > stst.out"))
+  (unless (fxzero? (system "./program > program.out"))
     (error 'execute "produced program exited abnormally")))
-
-(define (get-string)
-  (with-output-to-string
-    (lambda ()
-      (with-input-from-file "stst.out"
-        (lambda ()
-          (let f ()
-            (let ([c (read-char)])
-              (cond
-               [(eof-object? c) (void)]
-               [else (display c) (f)]))))))))
-
-(define (test-with-string-output test-id expr expected-output)
-   (run-compile expr)
-   (build)
-   (execute)
-   (unless (string=? expected-output (get-string))
-     (error 'test "output mismatch for test ~s, expected ~s, got ~s"
-        test-id expected-output (get-string))))
 
 (define (emit . args)
   (apply fprintf (compile-port) args)
   (newline (compile-port)))
-
