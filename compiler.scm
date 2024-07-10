d24ace39ede2da23451d86aba5c6c0fbb5b0fdd5
MJ Pooladkhay <mj@pky.me>
2024-07-10 12:15:13 +0100

    1.2 Immediate Constants - pass tests-1.2-req.scm
    Signed-off-by: MJ Pooladkhay <mj@pky.me>

--- compiler.scm
+++ compiler.scm
@@ -1,13 +1,55 @@
 (load "./tests/tests-driver.scm")
 (load "./tests/tests-1.1-req.scm")
+(load "./tests/tests-1.2-req.scm")
+
+(define char-shift 8)
+(define char-tag #x0F)   ;; #b00001111
+(define char-mask #xFF)  ;; #b11111111
+
+(define fx-shift 2)
+(define fx-tag #x00)     ;; #b00000000
+(define fx-mask #x03)    ;; #b00000011
+
+(define bool-f #x2F)     ;; #b00101111
+(define bool-t #x6F)     ;; #b01101111
+(define empty-list #x3F) ;; #b00111111
+(define wordsize 4)      ;; bytes
+
+(define fx-bits (- (* wordsize 8) fx-shift))
+(define fx-lower (- (expt 2 (- fx-bits 1))))
+(define fx-upper (- (expt 2 (- fx-bits 1)) 1))
+
+(define (fx? x)
+    (and (integer? x) (exact? x) (<= fx-lower x fx-upper)))
+
+(define (rep-fx x)
+    (ash x fx-shift))
+
+(define (rep-bool x)
+    (if (boolean=? x #t)
+        bool-t
+        bool-f))
+
+(define (rep-char x)
+    (bitwise-ior (ash (char->integer x) char-shift) char-tag))
+
+(define (immediate? x)
+    (or (fx? x) (boolean? x) (char? x) (null? x)))
+
+(define (rep-immedite x)
+    (cond
+        [(fx? x) (rep-fx x)]
+        [(boolean? x) (rep-bool x)]
+        [(null? x) empty-list]
+        [(char? x) (rep-char x)]))
 
 (define (emit-assembly x)
-  (unless (integer? x) (error 'emit-program "not an integer"))
-  (emit "  .text")
-  (emit "  .globl scheme_entry")
-  (emit "  .type scheme_entry, @function")
-  (emit "scheme_entry:")
-  (emit "  movl $~s, %eax" x)
-  (emit "  ret")
-  ;; Fixes linker error: missing .note.GNU-stack section implies executable stack
-  (emit "  .section	.note.GNU-stack,\"\",@progbits"))
+    (unless (immediate? x) (error 'emit-assembly "not an immediate"))
+    (emit "  .text")
+    (emit "  .globl scheme_entry")
+    (emit "  .type scheme_entry, @function")
+    (emit "scheme_entry:")
+    (emit "  movl $~s, %eax" (rep-immedite x))
+    (emit "  ret")
+    ;; Fixes linker error: missing .note.GNU-stack section implies executable stack
+    (emit "  .section	.note.GNU-stack,\"\",@progbits"))
--- runtime.c
+++ runtime.c
@@ -2,8 +2,70 @@
 
 extern int scheme_entry();
 
+/* define all scheme constants */
+#define BOOL_F 0x2F
+#define BOOL_T 0x6F
+#define FX_MASK 0x03
+#define FX_TAG 0x00
+#define FX_SHIFT 2
+#define EMPTY_LIST 0x3F
+#define CHAR_MASK 0xFF
+#define CHAR_TAG 0x0F
+#define CHAR_SHIFT 8
+
+/* all scheme values are of type ptrs */
+typedef unsigned int ptr;
+
+static void print_ptr(ptr x)
+{
+    if ((x & FX_MASK) == FX_TAG)
+    {
+        printf("%d\n", ((int)x) >> FX_SHIFT);
+    }
+    else if ((x & CHAR_MASK) == CHAR_TAG)
+    {
+        unsigned int ch = ((unsigned int)x) >> CHAR_SHIFT;
+        switch (ch)
+        {
+        case 9:
+            printf("#\\tab\n");
+            break;
+        case 10:
+            printf("#\\newline\n");
+            break;
+        case 13:
+            printf("#\\return\n");
+            break;
+
+        case 32:
+            printf("#\\space\n");
+            break;
+
+        default:
+            printf("#\\\%c\n", ch);
+            break;
+        }
+    }
+    else if (x == BOOL_T)
+    {
+        printf("#t\n");
+    }
+    else if (x == BOOL_F)
+    {
+        printf("#f\n");
+    }
+    else if (x == EMPTY_LIST)
+    {
+        printf("()\n");
+    }
+    else
+    {
+        printf("#<unknown 0x%08x>\n", x);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    printf("%d\n", scheme_entry());
+    print_ptr(scheme_entry());
     return 0;
 }
